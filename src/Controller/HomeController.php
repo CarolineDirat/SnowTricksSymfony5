f8eb7a2a71b470e05dd8e443d676b2d148221a98
Caroline Dirat <32294659+CarolineDirat@users.noreply.github.com>
2020-07-21 09:10:32 +0200

    add type return of index() method in src/Controller/HomeController.php

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,14 +3,17 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     /**
+     * Display the home page 
+     *
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(): Response
     {
         return $this->render('home/index.html.twig');
     }
