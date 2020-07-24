8ca7c7a7478125b0dfa22d2e91678e950aa0fb21
Caroline Dirat <32294659+CarolineDirat@users.noreply.github.com>
2020-07-24 09:50:53 +0200

    add a route to read a trick

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Trick;
+use App\Repository\TrickRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,8 +15,23 @@ class HomeController extends AbstractController
      *
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index(TrickRepository $trickRepo): Response
     {
-        return $this->render('home/index.html.twig');
+        // temporary code
+        $tricks = $trickRepo->findAll(); // There is actually only one trick on database
+        
+        return $this->render('home/index.html.twig', ['trick' => $tricks[0]]);
+    }
+
+    /**
+     * Display the page of a trick.
+     *
+     * @Route("/trick/{uuid}/{slug}", name="trick")
+     */
+    public function readTrick(Trick $trick ): Response
+    {
+        return $this->render('home/trick.html.twig', [
+            'trick' => $trick,
+        ]);
     }
 }
