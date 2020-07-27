17b6c0247072bfef6fde54ec49ce9336feb6490b
Caroline Dirat <32294659+CarolineDirat@users.noreply.github.com>
2020-07-27 17:02:41 +0200

    first method for 'Load more' button : BUT it's not ok !

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Trick;
 use App\Repository\TrickRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -26,12 +27,17 @@ class HomeController extends AbstractController
     /**
      * Display the page of a trick.
      *
-     * @Route("/trick/{uuid}/{slug}", name="trick")
+     * @Route("/trick/{uuid}/{slug}/{nb}", name="trick")
      */
-    public function readTrick(Trick $trick ): Response
+    public function readTrick(Trick $trick, Request $request, int $nb = 5): Response
     {
+        // reverse comments and define file name of profile picture of its user 
+        $comments = array_reverse($trick->getComments()->toArray());       
+        
         return $this->render('home/trick.html.twig', [
             'trick' => $trick,
+            'comments' => $comments,
+            'nb' => $nb,
         ]);
     }
 }
