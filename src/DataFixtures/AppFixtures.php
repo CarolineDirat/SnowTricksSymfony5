246bcd9e70049bd7bb009258c21a10f28deb77f6
Caroline Dirat <32294659+CarolineDirat@users.noreply.github.com>
2020-07-30 18:27:08 +0200

    refactoring  src/DataFixtures/AppFixtures.php

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -13,6 +13,7 @@ use DateTimeImmutable;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Faker\Factory;
+use Faker\Generator;
 use Ramsey\Uuid\Uuid;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\String\Slugger\AsciiSlugger;
@@ -55,18 +56,79 @@ class AppFixtures extends Fixture
         // put a profile picture on one user
         $users[1]->setProfile('squirrel.jpg');
         $manager->persist($users[1]);
-        // Indy Trick without pictures and video1
-        $indy = new Trick();
-        $indy->setUuid(Uuid::uuid4());
-        $indy->setName('Indy');
-        $indy->setDescription($faker->paragraphs(10, true));
-        $indy->setSlug(strtolower($slugger->slug($indy->getName())));
-        $indy->setCreatedAt(DateTimeImmutable::createFromMutable($faker->dateTimeThisMonth('now', 'Europe/Paris')));
-        $indy->setUpdatedAt($indy->getCreatedAt()->add(new DateInterval('P2D')));
-        $indy->setGroupTrick($groups[0]);
-        // comments
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // Indy Trick 
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////
+        $name = 'Indy';
+        $description = "La main arrière vient graber la carre frontside entre les pieds. Sur un saut droit c’est un Indy Grab, sur un hip ou un quarter en front c’est un frontside indy ou frontside grab alors que sur un saut en back (3.6 back par exemple) ça sera un backside Indy.";
+        $group = $groups[0];
+        $indy = $this->createTrickWithoutCommentsPicturesVideos($name, $description, $group, $faker, $slugger);
+        // add 12 comments to Indy trick
+        $this->addCommentsToTrick(12, $indy, $users, $manager, $faker);
+        // add Images
+        $this->addPicturesToTrick($indy, 7, $faker);
+        // add Videos
+        $videosData = [
+            '6QsLhWzXGu0' => 'YouTube',
+            'iKkhKekZNQ8' => 'YouTube', // @phpstan-ignore-line
+            '6yA3XqjTh_w' => 'YouTube',
+            'iKkhKekZNQ8' => 'YouTube',
+            'x2b3swg' => 'Dailymotion',
+            '19967156' => 'Vimeo',
+        ];
+        $this->addVideoToTrick($indy, $videosData);
+        $manager->persist($indy);
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // Canadian Bacon Trick 
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+        $manager->flush();
+    }
+    
+    /**
+     * createTrickWithoutCommentsPicturesVideos :
+     * create a trick without comments, pictures and videos neither
+     *
+     * @param  string $name
+     * @param  string $description
+     * @param  Group $group
+     * @return Trick
+     */
+    public function createTrickWithoutCommentsPicturesVideos(
+        string $name,
+        string $description,
+        Group $group,
+        Generator $faker,
+        AsciiSlugger $slugger
+    ): Trick {
+        $trick = new Trick();
+        $trick->setUuid(Uuid::uuid4());
+        $trick->setName($name);
+        $trick->setDescription($description);
+        $trick->setSlug(strtolower($slugger->slug($trick->getName())));
+        $trick->setCreatedAt(DateTimeImmutable::createFromMutable($faker->dateTimeThisMonth('now', 'Europe/Paris')));
+        $trick->setUpdatedAt($trick->getCreatedAt()->add(new DateInterval('P2D')));
+        $trick->setGroupTrick($group);
+
+        return $trick;
+    }
+    
+    /**
+     * addCommentsToTrick : add $number comments to a trick entity, from 2 users in an array
+     *
+     * @return void
+     */
+    public function addCommentsToTrick(
+        int $number,
+        Trick $trick,
+        array $users,
+        ObjectManager $manager,
+        Generator $faker
+    ): void {
         $comments = [];
-        for ($i = 0; $i < 12; ++$i) {
+        for ($i = 0; $i < $number; ++$i) {
             $comment = new Comment();
             $comment->setContent($faker->text(200));
             $comment->setCreatedAt((new DateTimeImmutable('2020-08-08 12:10:55'))->add(new DateInterval('PT'.$i.'H')));
@@ -82,36 +144,39 @@ class AppFixtures extends Fixture
             $comments[] = $comment;
         }
         foreach ($comments as $comment) {
-            $indy->addComment($comment);
+            $trick->addComment($comment);
         }
-        // Images
+    }
+    
+    /**
+     * addVideoToTrick
+     *
+     * @param  Trick $trick
+     * @param  array $videosData array = ['code' => 'service']
+     * @return void
+     */
+    public function addVideoToTrick(Trick $trick, array $videosData): void
+    {
+        foreach ($videosData as $code => $service) {
+            $video = new Video();
+            $video->setCode($code);
+            $video->setService($service);
+            $trick->addVideo($video);
+        }
+    }
+    
+    public function addPicturesToTrick(Trick $trick, int $number, Generator $faker): void
+    {
         $pictures = [];
-        for ($i = 1; $i <= 7; ++$i) {
+        for ($i = 1; $i <= $number; ++$i) {
             $picture = new Picture();
             $picture->setFilename('indy-'.$i.'.jpg');
             if ($i < 4) {
                 $picture->setAlt($faker->realText(40));
             }
-            $indy->addPicture($picture);
+            $trick->addPicture($picture);
             $pictures[] = $picture;
         }
-        $indy->setFirstPicture($pictures[3]);
-        // Videos
-        $videosData = [
-            '6QsLhWzXGu0' => 'YouTube',
-            'iKkhKekZNQ8' => 'YouTube', // @phpstan-ignore-line
-            '6yA3XqjTh_w' => 'YouTube',
-            'iKkhKekZNQ8' => 'YouTube',
-            'x2b3swg' => 'Dailymotion',
-            '19967156' => 'Vimeo',
-        ];
-        foreach ($videosData as $code => $service) {
-            $video = new Video();
-            $video->setCode($code);
-            $video->setService($service);
-            $indy->addVideo($video);
-        }
-        $manager->persist($indy);
-        $manager->flush();
+        $trick->setFirstPicture($pictures[1]);
     }
 }
