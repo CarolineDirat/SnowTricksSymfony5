2b7e2b2c3b237e2f2896a080abb3034b0b588bea
Caroline Dirat <32294659+CarolineDirat@users.noreply.github.com>
2020-09-12 17:27:56 +0200

    create src/FormHandler/RegistrationFormHandler.php to refactor RegistrationController::register()

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -4,15 +4,12 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\AppFormFactoryInterface;
-use App\Form\RegistrationFormType;
+use App\FormHandler\RegistrationFormHandler;
 use App\Security\EmailVerifier;
-use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Mime\Address;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
 
 class RegistrationController extends AbstractController
@@ -32,39 +29,13 @@ class RegistrationController extends AbstractController
     /**
      * @Route("/register", name="app_register")
      */
-    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
-    {
+    public function register(
+        Request $request,
+        RegistrationFormHandler $registrationFormHandler
+    ): Response {
         $user = new User();
         $form = $this->appFormFactory->create('registration', $user);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            // encode the plain password
-            $user->setPassword(
-                $passwordEncoder->encodePassword(
-                    $user,
-                    $form->get('plainPassword')->getData()
-                )
-            );
-
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($user);
-            $entityManager->flush();
-
-            // generate a signed url and email it to the user
-            $this->emailVerifier->sendEmailConfirmation(
-                'app_verify_email',
-                $user,
-                (new TemplatedEmail())
-                    ->from(new Address('mailer@snowtricks.test', 'Snowtricks'))
-                    ->to($user->getEmail())
-                    ->subject('Confirmation de votre Email')
-                    ->htmlTemplate('registration/confirmation_email.html.twig')
-            );
-            // do anything else you need here, like send an email
-            $this->addFlash('success', "Votre compte a été créé. 
-            Il reste à l'activer depuis le mail de confirmation que nous vous avons envoyé.");
-
+        if ($registrationFormHandler->isHandled($request, $form)) {
             return $this->redirectToRoute('home');
         }
 
@@ -88,7 +59,10 @@ class RegistrationController extends AbstractController
 
             return $this->redirectToRoute('app_register');
         }
-        $this->addFlash('success', 'Votre addresse mail a validée, et votre compte est activé.');
+        $this->addFlash(
+            'success',
+            'Votre addresse mail a validée, et votre compte est activé.'
+        );
 
         return $this->redirectToRoute('tricks');
     }
---
+++ src/FormHandler/RegistrationFormHandler.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\FormHandler;
+
+use App\Entity\User;
+use App\Security\EmailVerifier;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\HttpFoundation\Session\Attribute\AttributeBag;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Session\Storage\NativeSessionStorage;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class RegistrationFormHandler extends AbstractFormHandler
+{
+    private ManagerRegistry $manager;
+
+    private UserPasswordEncoderInterface $passwordEncoder;
+
+    private EmailVerifier $emailVerifier;
+
+    private Session $session;
+
+    public function __construct(
+        ManagerRegistry $manager,
+        UserPasswordEncoderInterface $passwordEncoder,
+        EmailVerifier $emailVerifier
+    ) {
+        $this->manager = $manager;
+        $this->passwordEncoder = $passwordEncoder;
+        $this->emailVerifier = $emailVerifier;
+        $this->session = new Session(new NativeSessionStorage(), new AttributeBag());
+    }
+
+    public function getEntityClass(): string
+    {
+        return User::class;
+    }
+
+    public function process(object $user): void
+    {
+        // encode the plain password
+        $user->setPassword(
+            $this->passwordEncoder->encodePassword(
+                $user,
+                $this->form->get('plainPassword')->getData()
+            )
+        );
+
+        $entityManager = $this->manager->getManager();
+        $entityManager->persist($user);
+        $entityManager->flush();
+        // generate a signed url and email it to the user
+        $this->emailVerifier->sendEmailConfirmation(
+            'app_verify_email',
+            $user,
+            (new TemplatedEmail())
+                ->from(new Address('mailer@snowtricks.test', 'Snowtricks'))
+                ->to($user->getEmail())
+                ->subject('Confirmation de votre Email')
+                ->htmlTemplate('registration/confirmation_email.html.twig')
+        );
+        // add a flash message to notify confirmation success
+        $this->session->getFlashBag()->add('success', "Votre compte a été créé. 
+            Il reste à l'activer depuis le mail de confirmation que nous vous avons envoyé.");
+    }
+}
